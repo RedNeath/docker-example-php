a7723e1878fb6cad74b3d0e5cea8f3d21a1984c9
RedNeath <theandre.bourry@ik.me>
2024-11-29 20:59:58 +0100

    [FEAT] Email sending

--- src/Controller/HealthController.php
+++ src/Controller/HealthController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 use OpenApi\Attributes as OA;
 use Nelmio\ApiDocBundle\Annotation\Model;
@@ -14,15 +17,36 @@ class HealthController extends AbstractController
 {
     #[Route('/api/health', name: 'app_health', methods: ['GET'])]
     #[OA\Response(
-        response: 200,
+        response: Response::HTTP_OK,
         description: 'Server health',
         content: new OA\JsonContent(ref: new Model(type: HealthDto::class))
     )]
     /**
      * Returns the server's health state.
      */
-    public function index(): JsonResponse
+    public function getHealth(): JsonResponse
     {
         return $this->json(new HealthDto('Welcome to your new controller!', 'src/Controller/HealthController.php'));
+    }
+
+    #[Route('/api/health-report', name: 'app_health_report', methods: ['POST'])]
+    #[OA\Response(
+        response: Response::HTTP_NO_CONTENT,
+        description: 'Email was sent to the maintainer'
+    )]
+    /**
+     * Sends a health report to the maintainer by email.
+     */
+    public function sendHealthReport(MailerInterface $mailer): Response
+    {
+        $health = new HealthDto('Welcome to your new controller!', 'src/Controller/HealthController.php');
+        $email = (new Email())
+            ->from('server@docker-example-web.org')
+            ->to('maintainer@docker-example-web.org')
+            ->subject('SERVER HEALTH REPORT')
+            ->html("<table><tbody><tr><td>Message</td><td>{$health->message}</td></tr><tr><td>Path</td><td>{$health->path}</td></tr></tbody></table>");
+        
+        $mailer->send($email);
+        return new Response(null, Response::HTTP_NO_CONTENT);
     }
 }
